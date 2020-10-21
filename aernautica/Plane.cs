e007797239532c1b102d8c7accc32d8003e2bb41
master-gunayu <p.panhofer@htlkrems.at>
2020-10-21 18:31:23 +0200

    hello

---
+++ aernautica/Plane.cs
@@ -0,0 +1,10 @@
+namespace aernautica {
+    public class Plane {
+        private int _speed;
+
+        public int Speed {
+            get => _speed;
+            set => _speed = value;
+        }
+    }
+}
